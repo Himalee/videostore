0e4ae27939fea4978bdd08cd3cbd29c536494b30
Himalee <himalee.tailor@gmail.com>
2018-11-30 11:40:18 +0000

    Refactor(using Martin Fowler - Refactoring): move switch statement to new method

--- src/main/java/StatementGenerator.java
+++ src/main/java/StatementGenerator.java
@@ -13,26 +13,14 @@ public class StatementGenerator {
             double 		thisAmount = 0;
             Rental 		each = (Rental)rentals.nextElement ();
 
-            // determines the amount for each line
-            switch (each.getMovie ().getPriceCode ()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented () > 2)
-                        thisAmount += (each.getDaysRented () - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented () * 3;
-                    if (each.getDaysRented() > 1)
-                        frequentRenterPoints ++;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented () > 3)
-                        thisAmount += (each.getDaysRented () - 3) * 1.5;
-                    break;
-            }
+
+            thisAmount = amountFor(each);
 
             frequentRenterPoints++;
+
+            if (each.getMovie ().getPriceCode () == Movie.NEW_RELEASE
+                    && each.getDaysRented () > 1)
+                frequentRenterPoints++;
 
             result += "\t" + each.getMovie ().getTitle () + "\t"
                     + String.valueOf (thisAmount) + "\n";
@@ -46,4 +34,27 @@ public class StatementGenerator {
 
         return result;
     }
+
+    private double amountFor(Rental each) {
+        double thisAmount = 0;
+        // determines the amount for each line
+        switch (each.getMovie ().getPriceCode ()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented () > 2)
+                    thisAmount += (each.getDaysRented () - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented () * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented () > 3)
+                    thisAmount += (each.getDaysRented () - 3) * 1.5;
+                break;
+        }
+
+        return thisAmount;
+    }
+
 }
